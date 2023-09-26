6c5ed6ebc2a7dc17b4d24525b6f645b563916fb3
GulievAnar <guliev@internet.ru>
2023-09-26 10:07:29 +0300

    Stylelint, i18next plugin for empty strings

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -2,6 +2,7 @@ import React, { type FC } from 'react'
 import styles from './Navbar.module.scss'
 import { classNames } from 'shared/lib/classNames/classNames'
 import { LinkComponent } from 'shared/ui/Link'
+import { useTranslation } from 'react-i18next'
 
 interface NavbarProps {
   className?: string
@@ -9,16 +10,17 @@ interface NavbarProps {
 
 export const Navbar: FC<NavbarProps> = (props) => {
   const { className } = props
+  const { t } = useTranslation('navbar')
   return (
-        <nav className={classNames(styles.nav, {}, [className ?? ''])}>
-            <div className={styles.nav__links}>
-                <LinkComponent to='/'>
-                    Main
-                </LinkComponent>
-                <LinkComponent to='/about'>
-                    About
-                </LinkComponent>
-            </div>
-        </nav>
+    <nav className={classNames(styles.nav, {}, [className ?? ''])}>
+      <div className={styles.nav__links}>
+        <LinkComponent to='/'>
+          {t('main')}
+        </LinkComponent>
+        <LinkComponent to='/about'>
+          {t('about')}
+        </LinkComponent>
+      </div>
+    </nav>
   )
 }
