6d5db8956df863cf58be96d26af533a03bb5c234
GulievAnar <guliev@internet.ru>
2023-10-30 09:59:16 +0300

    fix linter in test

--- src/shared/ui/Button/ui/Button.stories.tsx
+++ src/shared/ui/Button/ui/Button.stories.tsx
@@ -1,29 +1,28 @@
-import React from 'react';
-import type { Meta, StoryObj } from '@storybook/react';
-import { ButtonComponent } from "./Button";
-import { StyleDecorator } from 'shared/config/styleDecorator/styleDecorator';
+import React from 'react'
+import type { Meta, StoryObj } from '@storybook/react'
+import { ButtonComponent } from './Button'
+import { StyleDecorator } from 'shared/config/styleDecorator/styleDecorator'
 
 const meta = {
-    title: 'Example/ButtonComponent',
-    component: ButtonComponent,
-    decorators: [
-        (Story) => (
+  title: 'Example/ButtonComponent',
+  component: ButtonComponent,
+  decorators: [
+    (Story) => (
           <StyleDecorator>
             <Story />
           </StyleDecorator>
-        ),
-      ],
-    tags: ['autodocs'],
-} satisfies Meta<typeof ButtonComponent>;
+    )
+  ],
+  tags: ['autodocs']
+} satisfies Meta<typeof ButtonComponent>
 
-export default meta;
-type Story = StoryObj<typeof meta>;
+export default meta
+type Story = StoryObj<typeof meta>
 
 export const Primary: Story = {
-    args: {
-        className: 'default',
-        onClick: () => {console.log('click')},
-        children: <span>Test</span>,
-    },
-};
-
+  args: {
+    className: 'default',
+    onClick: () => { console.log('click') },
+    children: <span>{'Test'}</span>
+  }
+}
